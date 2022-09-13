688338d66d7145006017c5d5a6310e74e01a2134
Bamidele <alphawga@gmail.com>
2022-09-13 16:55:12 +0100

    init

---
+++ app.py
@@ -0,0 +1,37 @@
+#youtuber = "Alphawga Channel"
+#print ("subscribe to " + youtuber)
+# ("subscribe to {}".format(youtuber))
+#print (f"subscribe to {youtuber}")
+
+name= input("What is your name: ")
+color= input("What is your favorite color: ")
+color_reason1= input("First reason: ")
+color_reason2= input("Second reason: ")
+color_reason3= input("Third reason: ")
+
+animal= input("What is your favorite animal: ")
+animal_reason1= input("First reason: ")
+animal_reason2= input("Second reason: ")
+animal_reason3= input("Third reason: ")
+
+
+water= input("What is your favorite form of water: ")
+water_reason1= input("First reason: ")
+water_reason2= input("Second reason: ")
+water_reason3= input("Third reason: ")
+
+
+madlib = (f"Hi  {name},"
+          f" Let me tell you something about yourself,"
+          f" Your view of your personality is {color},"
+          f" You are  {color_reason1}, {color_reason2}, and {color_reason3}"
+
+          f" Your view of your perfect mate is {animal},"
+          f" you perfect mate has to be   {animal_reason1}, {animal_reason2} and {animal_reason3},"
+
+          f" Your view of your sexuality  is {water},"
+          f" you would love a relationship and sexual experience that is {water_reason1}, {water_reason2} and {water_reason3}"
+          )
+
+
+print(madlib)
